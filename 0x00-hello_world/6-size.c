c87f4779bdd770df566b759d95f3ae3a72cd49e4
koechMR <briankipkoech328@gmail.com>
2023-02-16 01:51:24 -0800

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-
-int main(void) 
-{	
+int main(void)
+{
 	int a;
 	long int b;
 	long long int c;
@@ -20,5 +19,4 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
-
 }
