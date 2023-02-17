a53bea107d571aad880c507931783b1080ccb901
koechMR <briankipkoech328@gmail.com>
2023-02-17 03:25:21 -0800

    Alphabet without e q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* File: 4-print_alphabet.c
+ */
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i!= 'q')
+			putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
