8f5658c66c012d047e70878a92d35c11a372f746
koechMR <briankipkoech328@gmail.com>
2023-02-21 05:42:36 -0800

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,13 +9,19 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
 		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
 		return (-1);
 	}
 	_putchar('0');
+	_putchar(',');
+	_putchar(' ');
 	return ('0');
 }
