2c17e9529489ee8f04e1bbe8480991725ee17bab
koechMR <briankipkoech328@gmail.com>
2023-02-23 00:30:21 -0800

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *print_numbers - print numbers
+  *Return: void
+  */
+void print_numbers(void)
+{
+	char i = 0;
+
+	for (i = '0'; i <= '9'; i++)
+
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
