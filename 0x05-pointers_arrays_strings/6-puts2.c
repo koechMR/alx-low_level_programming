8f02b028e2066c22a85f6f22e6b50c6e8448a55d
koechMR <briankipkoech328@gmail.com>
2023-02-27 02:42:15 -0800

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  *puts2 -  prints every other character of a string
+  *@str: parameter being checked
+  *Return: 0
+  */
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		if (a % 2 == 0)
+		{
+			_putchar(str[a]);
+		}
+		a++;
+	}
+	_putchar('\n');
+}
