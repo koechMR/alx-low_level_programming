226984bdeb13e72e98e98776f39be99d3598359b
koechMR <briankipkoech328@gmail.com>
2023-03-14 10:37:17 +0300

    concatinating

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*_concat - functin that concatenate two srings
+*@s1: first input
+*@s2: second input
+*Return: NULL if failure
+*/
+char *str_concat(char *s1, char *s2)
+{
+char *concat_str;
+int index, concat_index = 0, len = 0;
+
+if (s1 == NULL)
+s1 = "";
+
+if (s2 == NULL)
+s2 = "";
+
+for (index = 0; s1[index] || s2[index]; index++)
+len++;
+
+concat_str = malloc(sizeof(char) * len);
+
+if (concat_str == NULL)
+return (NULL);
+
+for (index = 0; s1[index]; index++)
+concat_str[concat_index++] = s1[index];
+
+for (index = 0; s2[index]; index++)
+concat_str[concat_index++] = s2[index];
+
+return (concat_str);
+}
