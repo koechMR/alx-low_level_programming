1bb722b054adebb4f2b304c9dd42ba966454c122
koechMR <briankipkoech328@gmail.com>
2023-03-30 15:31:10 +0300

    list length

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LIST_H
 #define LIST_H
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -11,9 +12,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
