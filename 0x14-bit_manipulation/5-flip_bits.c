6e5f2793afdd457a5cc2aef9995792345dceea8d
koechMR <briankipkoech328@gmail.com>
2023-04-08 10:04:27 +0300

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *flip_bits - functiont that returns number of bits
+ *@n: parameter checked
+ *@m: interger
+ *Return: number of bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor_result = n ^ m;
+	unsigned int count = 0;
+
+	while (xor_result != 0)
+	{
+		count += xor_result & 1;
+		xor_result >>= 1;
+	}
+	return (count);
+}
+
