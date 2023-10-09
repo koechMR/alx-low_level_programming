669128af5bd99f6e7ce148ac54de0a9cb53a39a7
koechMR <briankipkoech328@gmail.com>
2023-10-09 09:27:13 +0300

    linear search in searching algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "search_algos.h"
+
+int linear_search(int *array, size_t size, int value)
+{
+int i;
+
+if (array == NULL)
+{
+return (-1);
+}
+
+for (i = 0; (size_t)i < size; i++)
+{
+printf("Value checked array[%d] = [%d]\n", i, array[i]);
+
+if (array[i] == value)
+{
+return (i);
+}
+}
+
+return (-1);
+}
