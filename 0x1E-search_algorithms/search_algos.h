bb2f688d781b5581ff82df8d57086404d78320fc
koechMR <briankipkoech328@gmail.com>
2023-10-10 10:51:59 +0300

    space complexity

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,15 +4,14 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
- *
  * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
+ * @index: index of the node in list
+ * @next: pointer to next node
  *
- * Description: singly linked list node structure
- * for Holberton project
+ * Description: singly linked list node
  */
 typedef struct listint_s
 {
@@ -21,28 +20,8 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**
- * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
- * for Holberton project
- */
-typedef struct skiplist_s
-{
-	int n;
-	size_t index;
-	struct skiplist_s *next;
-	struct skiplist_s *express;
-} skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
-skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif /* SEARCH_ALGOS_H */
